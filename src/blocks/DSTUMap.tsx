964951277dc5a554173f60b3ff608484876776f3
Arnovec <dgaoling31@gmail.com>
2021-04-13 14:17:54 +0300

    Change overlay and add icon

--- src/blocks/DSTUMap.tsx
+++ src/blocks/DSTUMap.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
-import PointList from './PointsList'
-import Popup from './Popup'
+import PointList from './PointsList';
+import Popup from './Popup';
 import 'ol/ol.css';
-import OverlayPositioning from 'ol/OverlayPositioning'
+import OverlayPositioning from 'ol/OverlayPositioning';
 import { Map, View, Overlay } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import Feature from 'ol/Feature';
 import VectorLayer from 'ol/layer/Vector';
-import { Point } from 'ol/geom';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { Point, SimpleGeometry } from 'ol/geom';
+import { Style, Icon } from 'ol/style';
 
 export default function DSTUMap() {
 
@@ -17,67 +17,68 @@ export default function DSTUMap() {
     {
       coordinates: [4420571, 5981353],
       title: "Парк ДГТУ",
-      description: "Студенческий парк"
+      description: "Студенческий парк",
+      type: "parck"
     },
     {
       coordinates: [4420802, 5981235],
       title: "Общежитие ДГТУ",
-      description: ""
+      description: "",
+      type: "accommodation"
     },
     {
       coordinates: [4420737, 5980711],
       title: "Остановка пл.Гагарина",
-      description: "93, 25"
+      description: "93, 25",
+      type:"bus-stop"
     },
     {
       coordinates: [4420739, 5980690],
       title: "Остановка пл.Гагарина",
-      description: "93"
+      description: "93",
+      type:"bus-stop"      
     },
     {
       coordinates: [4420857.6639, 5981127.4538],
       title: "Остановка общежитие РИСИ",
-      description: "1, 5, 22, 33"
+      description: "1, 5, 22, 33",
+      type:"bus-stop"
     },
     {
       coordinates: [4420783, 5980652],
       title: "Шашлычная",
-      description: ""
-    },
+      description: "",
+      type:"eat"
+    }
   ]
 
-  const [OverlayInformation, setOverlayIformation] = React.useState({
-    title: "1",
-    description: "2"
-  })
-
   const myFeatures = MapObjects.map(elem => {
-    return new Feature({
+    const feature = new Feature({
       geometry: new Point(elem.coordinates),
       title: elem.title,
       description: elem.description
+    });
+    feature.setStyle(
+      new Style({
+      image: new Icon({
+        src: `/image/${elem.type}.png`,
+      })
     })
+  );
+    return feature;
   })
 
   const source = new VectorSource({
     features: myFeatures
   })
 
-  const bluePointCircle = new Style({
-    image: new CircleStyle({
-      radius: 10,
-      stroke: new Stroke({
-        color: '#fff',
-      }),
-      fill: new Fill({
-        color: '#3399CC',
-      }),
-    })
-  })
-
   const clusters = new VectorLayer({
     source,
-    style: bluePointCircle
+  })
+
+  const view = new View({
+    center: [4420594, 5981270],
+    zoom: 17,
   })
 
   const map = new Map({
@@ -87,10 +88,7 @@ export default function DSTUMap() {
       }),
       clusters
     ],
-    view: new View({
-      center: [4420594, 5981270],
-      zoom: 17,
-    }),
+    view,
   });
 
   const popup = new Overlay({
@@ -107,19 +105,35 @@ export default function DSTUMap() {
     const popupTitle = document.getElementById("popup_title")!;
     const popupDescription = document.getElementById("popup_description")!;
 
+    function showOverlay(feature: Feature){
+      popup.setPosition(feature.get("geometry").flatCoordinates);
+      popup.getElement()!.style.display = "block";
+      popupTitle.innerText = feature.get("title");
+      popupDescription.innerText = feature.get("description");
+    }
+
+    const zoomsToPoint = [...document.getElementsByClassName("ant-col")]//! as HTMLElement[]
+    zoomsToPoint.map((elem, id) =>{
+      elem.addEventListener(
+        'click',
+        () =>{
+          const feature = source.getFeatures()[id];
+          const polygon  = feature.getGeometry()! as SimpleGeometry;
+          view.fit(polygon, {padding: [0, 0, 0, 0], minResolution: 1});
+          showOverlay(feature);
+        }
+      )
+    })
+
     map.on('click', function (evt) {
+      console.log(evt.pixel);
       const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
         return feature;
       });
       if (feature) {
-        console.log("True");
-        popup.setPosition(feature.get("geometry").flatCoordinates);
-        popup.getElement()!.style.display = "block";
-        popupTitle.innerText = feature.get("title");
-        popupDescription.innerText = feature.get("description");
+        showOverlay(feature! as Feature);
 
       } else {
-        console.log("false");
         popup.getElement()!.style.display = "none";
       }
     })
@@ -130,7 +144,7 @@ export default function DSTUMap() {
     <>
       <div id="map" style={{ width: "100%", height: "100%" }}>
         <PointList arrayPoints={MapObjects} />
-        <Popup information={OverlayInformation} />
+        <Popup />
       </div>
     </>
   );
--- src/blocks/Popup.tsx
+++ src/blocks/Popup.tsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import './sass/mapContent.sass'
 
-export default class Popup extends React.Component<{
-    information: {
-        title: string,
-        description: string
-    }
-}> {
+export default class Popup extends React.Component {
     render() {
         return (
             <div id="popup">
                 <div className="popup_content">
-                    <p id="popup_title">
-                        {this.props.information.title}
-                    </p>
-                    <p id="popup_description">
-                        {this.props.information.description}
-                    </p>
+                    <p id="popup_title"></p>
+                    <p id="popup_description"></p>
                 </div>
                 <div className="popover_arrow"></div>
             </div>
